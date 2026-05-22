6750fe0008bb87988a0df7a4edd46f90636e4374
deepanshutr <52166434+deepanshutr@users.noreply.github.com>
2026-05-22 11:51:40 +0530

    chore: scaffold esptouch library repo (pyproject, LICENSE, gitignore)

---
+++ esptouch/__init__.py
@@ -0,0 +1,8 @@
+"""ESP-TOUCH (Espressif SmartConfig) Wi-Fi provisioning library.
+
+Public API is finalised in the run()-wrapper task. This module is
+import-stable: wiz-core and tuya-core depend on
+`from esptouch import run, NewBulb, EsptouchError`.
+"""
+
+from __future__ import annotations
